08f05e7c36a658da292c24f99ff867fc45c207ef
newthiagoassisk8 <assisthiago2303@gmail.com>
2025-06-14 11:01:18 -0300

    pequena mudanca para testar meu novo dotfile

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from "./app";
 import { env } from "./configs/envs.config";
 
 app.listen(env.PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${env.PORT}`);
+    console.log(`🚀 Servidor web rodando em http://localhost:${env.PORT}`);
 });
 
